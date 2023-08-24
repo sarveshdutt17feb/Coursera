ebf4a0aadb89167635ee049821730b90a864ebe3
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2023-08-24 14:01:28 +0530

    commit

---
+++ coursera/server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/admin", adminRouter)
+app.use("/user", userRouter)
+app.get("/", (req, res) => res.json({msg: "hello world after the class"}));
+
+// Connect to MongoDB
+// DONT MISUSE THIS THANKYOU!!
+// mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+mongoose.connect('mongodb+srv://sarveshdutt40:tWPBMcfOnzs9hbxU@cluster0.gunxz8q.mongodb.net/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+
+app.listen(3000, () => console.log('Server running on port 3000'));
